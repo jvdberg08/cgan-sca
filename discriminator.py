bcd0355637e88238ef9c82c178b8846bfdb02f83
Joren van den Berg <jvdberg080403@gmail.com>
2023-05-08 18:07:34 +0200

    update gen and disc for new experiments

--- discriminator.py
+++ discriminator.py
@@ -4,15 +4,6 @@ from tensorflow.keras.layers import *
 def get_discriminator_layers(discriminator_idx, input_layer):
     gen = input_layer
     if discriminator_idx == 1:
-        gen = Dense(150, activation='elu', kernel_initializer='he_uniform')(gen)
-        gen = Dropout(0.3)(gen)
-        gen = Dense(150, activation='elu', kernel_initializer='he_uniform')(gen)
-        gen = Dropout(0.3)(gen)
-        gen = Dense(150, activation='elu', kernel_initializer='he_uniform')(gen)
-        gen = Dropout(0.3)(gen)
-        gen = Dense(150, activation='elu', kernel_initializer='he_uniform')(gen)
-        gen = Dropout(0.3)(gen)
-    elif discriminator_idx == 2:
         gen = Dense(250, activation='elu', kernel_initializer='he_uniform')(gen)
         gen = Dropout(0.3)(gen)
         gen = Dense(250, activation='elu', kernel_initializer='he_uniform')(gen)
